5ad0fb8a7bc918cf44cbf0c029468871b04108a8
neonew <neonew@gmail.com>
2019-11-25 18:00:25 +0100

    Support for slow-mo videos (fps 240)

--- video.sh
+++ video.sh
@@ -3,7 +3,8 @@
 # scale -8: Some codecs require the size of width and height to be a multiple of n.
 # See https://trac.ffmpeg.org/wiki/Scaling
 # scale max 720: Do not upscale videos
-ffmpeg -hide_banner -loglevel panic -i "$1" -filter:v scale="-8:'min(720,ceil(ih/8)*8)'" -c:v libx265 -x265-params log-level=error -crf 28 -c:a aac -b:a 96k "$2"
+# max_muxing_queue_size: See https://stackoverflow.com/questions/49686244/ffmpeg-too-many-packets-buffered-for-output-stream-01
+ffmpeg -hide_banner -loglevel panic -i "$1" -max_muxing_queue_size 9999 -filter:v scale="-8:'min(720,ceil(ih/8)*8)'" -c:v libx265 -x265-params log-level=error -crf 28 -c:a aac -b:a 96k "$2"
 
 # Hardware acceleration via VAAPI
 # However, bitrate cannot be set manually due to a bug: https://forum.kde.org/viewtopic.php?f=272&t=162168
