04fd408339e460ae4d0929b1cfe3b9cc187cc8e0
neonew <neonew@gmail.com>
2024-03-01 15:34:24 +0100

    Add more filetypes (.md, .svg, .gpg)

--- archive.sh
+++ archive.sh
@@ -51,7 +51,7 @@ function copyFile {
   elif [[ "${LOWERCASE_FILENAME}" =~ ^.*\.(avi|mp4|mpg|mpeg|mov)$ ]]; then
     # Already handled before
     :
-  elif [[ "${LOWERCASE_FILENAME}" =~ ^.*\.(txt|html|mhtml|xhtml|pdf|doc|docx|xls|xlsx|odt|ods)$ ]]; then
+  elif [[ "${LOWERCASE_FILENAME}" =~ ^.*\.(txt|md|html|mhtml|xhtml|pdf|doc|docx|xls|xlsx|odt|ods|svg|gpg)$ ]]; then
     convertFile "$0" ./copy.sh "${0##*.}"
   else
     echo "File not copied (unknown type): ${0}"
